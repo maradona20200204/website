feff1355d0e20ac8d03dd6b3105027a7e06bc363
miaosi2018@sari.ac.cn <maradona20200204@gmail.com>
2020-12-25 08:28:25 +0800

    Successful

--- server.py
+++ server.py
@@ -17,12 +17,14 @@ rootfile = config_dict['rootfile']
 prefixstr = config_dict['prefixstr']
 protocol = config_dict['protocol']
 static_file = config_dict["static_file"]
+log_file = config_dict['log_file']
+data_latest_file = config_dict['data_latest_file']
+data_cache_file = config_dict['data_cache_file']
+gpu_latest_file = config_dict['gpu_latest_file']
+use_websocket = True if config_dict['use_websocket'] == 'True' else False
 
 
-def writelog(content, path='log/log.txt', logtime=True, breakline=True, ip='',prefixstr=prefixstr):
-    '''
-    I think the logging module is difficult to use
-    '''
+def writelog(content, path=log_file, logtime=True, breakline=True, ip='',prefixstr=prefixstr):
     if logtime:
         if ip:
             content = prefixstr+' [' + time.strftime('%Y-%m-%d %H:%M:%S') + ', ip = %s ]'%ip + content
@@ -77,9 +79,6 @@ class HelloworldHandler(tornado.web.RequestHandler):
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     cache = None
     
-    def check_origin(self, origin):  
-        return True 
-    
     def open(self):
         WebSocketHandler.cache = self
         
@@ -88,8 +87,8 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         
     @classmethod
     def send_updates(cls, message=None):
-        #print(WebSocketHandler.cache)
-        if WebSocketHandler.cache:
+        #print(time.strftime('%Y-%m-%d %H:%M:%S'), WebSocketHandler.cache, message)
+        if WebSocketHandler.cache and use_websocket:
             if not message:
                 WebSocketHandler.cache.write_message("Hello, world! from %s:%s"%(ip, port))
             else:
@@ -101,10 +100,71 @@ class GPUInfoHandler(tornado.web.RequestHandler):
         try:
             info = self.get_argument('gpuinfo', 'NAK')
             if info == 'NAK':
-                WebSocketHandler.send_updates({'status':'False', 'message':info})
+                data = {'status':'False', 'message':info, 'type':'GPU'}
             else:
-                WebSocketHandler.send_updates({'status':'True', 'message':info})
+                data = {'status':'True', 'message':info, 'type':'GPU'}
+            WebSocketHandler.send_updates(data)
+            with open(gpu_latest_file, 'w') as f:
+                json.dump(data, f)
             self.write("ACK")
+        except:
+            #print(traceback.format_exc())
+            self.write("NAK: " + traceback.format_exc())
+            
+            
+class InfoHandler(tornado.web.RequestHandler):
+    def post(self):
+        try:
+            epoch = self.get_argument('epoch', 'NAK')
+            project = self.get_argument('project', 'NAK')
+            data = self.get_argument('data', 'NAK')
+            dataid = self.get_argument('dataid', 'NAK')
+            if epoch != 'NAK':
+                try:
+                    epoch = int(epoch)
+                except:
+                    epoch = 'NAK'
+            if epoch == 'NAK' or project == 'NAK' or data == 'NAK' or dataid == 'NAK':
+                message = {'content': 'NAK', 'latest': 'NAK'}
+                senddata = {'status':'False', 'message': message, 'type':'Info', 'protocol':'websocket'}
+                with open(data_latest_file, 'w') as f:
+                    json.dump(senddata, f)
+                WebSocketHandler.send_updates(senddata)
+            else:
+                latest = 'Latest news (dataid=%s): project %s, epoch %s, data %s'%(dataid, project, epoch, data)
+                print(latest)
+                contentjson = {'epoch': epoch, 'project': project, 'data':data, 'dataid':dataid, 'time':time.strftime('%Y-%m-%d %H:%M:%S')}
+                message =  {'content': contentjson, 'latest': latest}
+                senddata = {'status':'True', 'message': message,'type':'Info', 'protocol':'websocket'}
+                with open(data_latest_file, 'w') as f:
+                    json.dump(senddata, f)
+                WebSocketHandler.send_updates(senddata)  
+            self.write("ACK")
+        except:
+            print(traceback.format_exc())
+            self.write("NAK: " + traceback.format_exc())
+
+            
+class QueryHandler(tornado.web.RequestHandler):
+    def post(self):
+        try:
+            datatype = self.get_argument('type', 'NAK')
+            assert datatype in ['NAK', 'GPU', 'Info']
+            if datatype == 'NAK':
+                self.write({'type':'NAK', 'result':'', 'cache':'None', 'status':'False', 'protocol':'http/https'})
+                return
+            else:
+                if datatype == 'GPU':
+                    file = gpu_latest_file
+                else:
+                    file = data_latest_file
+                cache_file_exists = False
+                response = ''
+                if os.path.isfile(file):
+                    with open(file, 'r') as f:
+                        response = f.read()
+                    cache_file_exists = True
+                self.write({'type':datatype, 'response':response, 'cache':str(cache_file_exists), 'status':'True', 'protocol':'http/https'})
         except:
             self.write("NAK: " + traceback.format_exc())
             
@@ -112,7 +172,7 @@ class GPUInfoHandler(tornado.web.RequestHandler):
 def main():
     tornado.options.parse_command_line()
     application = tornado.web.Application(
-        [("/", MainHandler), (r"/(?i)ping|/(?i)hello", HelloworldHandler),('/websocket', WebSocketHandler), ('/gpu', GPUInfoHandler)],static_path=os.path.join(os.path.dirname(__file__), "static")
+        [("/", MainHandler), (r"/(?i)ping|/(?i)hello", HelloworldHandler),('/websocket', WebSocketHandler), ('/gpu', GPUInfoHandler), ('/info', InfoHandler), ('/query', QueryHandler)], static_path=os.path.join(os.path.dirname(__file__), "static")
         )
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(port)
